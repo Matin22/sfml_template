315a78fe4c3efc689cf0e5279f5ed66a5228fc1a
Matin22 <gholami.matin@gmail.com>
2025-08-08 23:04:14 +0200

    change window size

--- src/config.hpp
+++ src/config.hpp
@@ -4,7 +4,7 @@
 namespace conf
 {
     // Window config
-    sf::Vector2u const WINDOW_SIZE = {1920, 1080};
+    sf::Vector2u const WINDOW_SIZE = {800, 600};
     sf::Vector2f const fWINDOW_SIZE = static_cast<sf::Vector2f>(WINDOW_SIZE);
     uint32_t MAX_FRAMERATE = 60;
     float const dt = 1.0f / static_cast<float>(MAX_FRAMERATE);
